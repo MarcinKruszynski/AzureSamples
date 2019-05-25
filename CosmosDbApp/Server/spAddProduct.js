c8e17168e27619124fbc7dede86d74c4882fc7a3
MarcinKruszynski <martin.kruszynski@gmail.com>
2019-05-25 10:27:36 +0200

    Create spAddProduct.js

---
+++ CosmosDbApp/Server/spAddProduct.js
@@ -0,0 +1,18 @@
+﻿function spAddProduct(docToCreate) {
+    if (docToCreate.shipping === undefined || docToCreate.shipping.width === undefined) {
+        throw new Error('Expected document to contain docToCreate.shipping.width.');
+    }
+
+    var context = getContext();
+    var collection = context.getCollection();
+    var response = context.getResponse();
+
+    collection.createDocument(collection.getSelfLink(), docToCreate, {},
+        function (err, docCreated) {
+            if (err) {
+                throw new Error('Error creating document: ' + err.Message);
+            }
+            response.setBody(docCreated);
+        }
+    );
+}
