f2a9e3b2ddfc28270f0d7c551c4491df44d2604e
MarcinKruszynski <martin.kruszynski@gmail.com>
2019-05-27 00:12:22 +0200

    add Azure Redis Cache

---
+++ Redis/RedisCacheApp/CustomerBasket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedisCacheApp
+{
+    public class CustomerBasket
+    {
+        public string BuyerId { get; set; }
+        public List<BasketItem> Items { get; set; }
+
+        public CustomerBasket(string customerId)
+        {
+            BuyerId = customerId;
+            Items = new List<BasketItem>();
+        }
+    }
+}
---
+++ Redis/RedisCacheApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RedisCacheApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string connectionString = "emkaredis.redis.cache.windows.net:6380,password=lfpPyiT0G2eBXyQVA57+mJbLSBSYR4Klvvrg6biVLMs=,ssl=True,abortConnect=False";
+
+            using (var cache = ConnectionMultiplexer.Connect(connectionString))
+            {
+                IDatabase db = cache.GetDatabase();
+
+
+                bool setValue = await db.StringSetAsync("test:key", "aaa");
+                Console.WriteLine($"SET: {setValue}");
+
+                string getValue = await db.StringGetAsync("test:key");
+                Console.WriteLine($"GET: {getValue}");
+
+
+                setValue = await db.StringSetAsync("counter", "100");
+                Console.WriteLine($"SET: {setValue}");
+
+                long newValue = await db.StringIncrementAsync("counter", 50);
+                Console.WriteLine($"INCR new value = {newValue}");
+
+                getValue = await db.StringGetAsync("counter");
+                Console.WriteLine($"GET: {getValue}");
+
+
+                var basket = new CustomerBasket("144");
+                basket.Items.Add(new BasketItem { Id = "1", ProductId = "26", ProductName = "Kubek Kamerzysta", UnitPrice = 29.9m, Quantity = 2 });
+                basket.Items.Add(new BasketItem { Id = "2", ProductId = "33", ProductName = "T-shirt Kamerzysta", UnitPrice = 59m, Quantity = 1 });
+
+
+                string serializedValue = Newtonsoft.Json.JsonConvert.SerializeObject(basket);
+                bool added = await db.StringSetAsync("basket:144-26:2-33:1", serializedValue);
+
+                var cbStr = await db.StringGetAsync("basket:144-26:2-33:1");
+                var cb = Newtonsoft.Json.JsonConvert.DeserializeObject<CustomerBasket>(cbStr.ToString());
+                Console.WriteLine(cb.BuyerId); 
+
+
+                var result = await db.ExecuteAsync("ping");
+                Console.WriteLine($"PING = {result.Type} : {result}");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
