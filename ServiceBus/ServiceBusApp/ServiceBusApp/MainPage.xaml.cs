364aedd9f9bb501ef0a568681419ef76e3caeafd
MarcinKruszynski <martin.kruszynski@gmail.com>
2019-06-11 00:32:08 +0200

    service bus queue - draft

---
+++ ServiceBus/ServiceBusApp/ServiceBusApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ServiceBusLib;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ServiceBusApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }        
+
+        private async void SendQueue_Click(object sender, RoutedEventArgs e)
+        {
+            await ServiceBusUtils.SendMessage(sendQueueTxt.Text);
+        }
+
+        private async void ReadQueue_Click(object sender, RoutedEventArgs e)
+        {
+            await ServiceBusUtils.ReceiveMessage(text => readQueueTxt.Text = text);
+        }
+    }
+}
---
+++ ServiceBus/ServiceBusLib/ServiceBusUtils.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBusLib
+{
+    public static class ServiceBusUtils
+    {
+        const string ServiceBusConnectionString = "";
+        const string QueueName = "salesmessages";
+        static IQueueClient queueClient;
+
+        static Action<string> action;
+
+        public static async Task SendMessage(string message)
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+           
+            var encodedMessage = new Message(Encoding.UTF8.GetBytes(message));
+            await queueClient.SendAsync(encodedMessage);
+
+            await queueClient.CloseAsync();
+        }
+
+
+        public static async Task ReceiveMessage(Action<string> a)
+        {
+            action = a;
+
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            RegisterMessageHandler();
+
+            await queueClient.CloseAsync();
+        }
+
+        static void RegisterMessageHandler()
+        {            
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {                
+                MaxConcurrentCalls = 1,                
+                AutoComplete = false
+            };
+            
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{body}");
+
+            action?.Invoke(body);
+
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
