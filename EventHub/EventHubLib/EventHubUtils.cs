e7f60bf68dc71e898c1880184ea0765dcbe815f1
MarcinKruszynski <martin.kruszynski@gmail.com>
2019-06-15 17:30:40 +0200

    az event hub app

---
+++ EventHub/EventHubLib/EventHubUtils.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubLib
+{
+    public static class EventHubUtils
+    {
+        private static EventHubClient eventHubClient;
+        private const string EventHubConnectionString = "";
+        private const string EventHubName = "emkaeventhub";
+
+        private const string StorageContainerName = "{Storage account container name}";
+        private const string StorageAccountName = "{Storage account name}";
+        private const string StorageAccountKey = "{Storage account key}";
+
+        private static readonly string StorageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", StorageAccountName, StorageAccountKey);
+
+        private static EventProcessorHost eventProcessorHost;
+
+        public static async Task SendMessages()
+        {
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(EventHubConnectionString)
+            {
+                EntityPath = EventHubName
+            };
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            await SendMessagesToEventHub(100);
+
+            await eventHubClient.CloseAsync();
+        }
+
+        
+        private static async Task SendMessagesToEventHub(int numMessagesToSend)
+        {
+            for (var i = 0; i < numMessagesToSend; i++)
+            {
+                try
+                {
+                    var message = $"Message {i}";                    
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch(Exception ex) 
+                {                    
+                }
+
+                await Task.Delay(10);
+            }            
+        }
+
+
+        public static async Task ReadMessages(Action<string> a)
+        {
+            eventProcessorHost = new EventProcessorHost(
+               EventHubName,
+               PartitionReceiver.DefaultConsumerGroupName,
+               EventHubConnectionString,
+               StorageConnectionString,
+               StorageContainerName);
+
+            SimpleEventProcessor.action = a;
+
+            await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();            
+        }
+
+        public static async Task UnregisterEventProcessor()
+        {
+            if (eventProcessorHost != null)
+                await eventProcessorHost.UnregisterEventProcessorAsync();
+        }
+    }
+}
---
+++ EventHub/EventHubLib/SimpleEventProcessor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubLib
+{
+    public class SimpleEventProcessor : IEventProcessor
+    {
+        public static Action<string> action;        
+
+        public Task CloseAsync(PartitionContext context, CloseReason reason)
+        {            
+            return Task.CompletedTask;
+        }
+
+        public Task OpenAsync(PartitionContext context)
+        {            
+            return Task.CompletedTask;
+        }
+
+        public Task ProcessErrorAsync(PartitionContext context, Exception error)
+        {            
+            return Task.CompletedTask;
+        }
+
+        public Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach (var eventData in messages)
+            {
+                var data = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+
+                action?.Invoke(data);
+            }
+
+            return context.CheckpointAsync();
+        }
+    }
+}
