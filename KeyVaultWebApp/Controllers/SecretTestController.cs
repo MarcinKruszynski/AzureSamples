8c28ff621285ccfe0bc2bb38ef5de0e66fb19b27
MarcinKruszynski <martin.kruszynski@gmail.com>
2019-06-16 10:15:28 +0200

    az key vault

---
+++ KeyVaultWebApp/Controllers/SecretTestController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace KeyVaultWebApp.Controllers
+{
+    [Route("api/[controller]")]    
+    public class SecretTestController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SecretTestController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var secretName = "SecretPassword";
+            var secretValue = _configuration[secretName];
+
+            if (secretValue == null)
+            {
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    $"Error: No secret named {secretName} was found...");
+            }
+            else
+            {
+                return Content($"Secret value: {secretValue}" +
+                    Environment.NewLine + Environment.NewLine +
+                    "This is for testing only! Never output a secret " +
+                    "to a response or anywhere else in a real app!");
+            }
+        }
+    }
+}
